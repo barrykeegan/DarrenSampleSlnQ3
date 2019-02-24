5ac022ebed123da51ffa3ad72d1f6500a680084a
barrykeegan <barry.keegan@gmail.com>
2019-02-24 18:21:59 +0000

    Add project files.

---
+++ Q3/Alumni.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Q3
+{
+    public class Alumni
+    {
+        public string CourseCode { get; }
+        public string FirstName { get; }
+        public string Surname { get; }
+        public DateTime GradYr { get; }
+        public DateTime StartYr { get; }
+
+        public Alumni(string courseCode, string firstName, string surname, DateTime gradYr, DateTime startYr)
+        {
+            if(gradYr > DateTime.Now)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if((gradYr.Year - startYr.Year) > 6)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if(courseCode == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if(!Regex.IsMatch(courseCode, "^(UG)|(PG)") || courseCode.Length > 8)
+            {
+                throw new ArgumentException("Invalid argument", "courseCode");
+            }
+
+            CourseCode = courseCode;
+            FirstName = firstName;
+            Surname = surname;
+            GradYr = gradYr;
+            StartYr = startYr;
+        }
+    }
+
+    public class AlumnusList
+    {
+        public List<Alumni> Alumnus { get; }
+
+        public AlumnusList()
+        {
+            Alumnus = new List<Alumni>();
+        }
+
+        public void AddAlumni(Alumni alumni)
+        {
+            if(alumni == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            Alumnus.Add(alumni);
+        }
+
+        public void AddAlumnus(Alumni[] alumnus)
+        {
+            if (alumnus == null)
+            {
+                throw new ArgumentNullException();
+            }
+            foreach (Alumni alum in alumnus)
+            {
+                if(alum == null)
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+            Alumnus.AddRange(alumnus);
+        }
+
+        public bool RemoveAlumni(Alumni alumni)
+        {
+            if(Alumnus.Count() == 0)
+            {
+                throw new ArgumentException("Alumnus List was empty, nothing to be removed");
+            }
+            return Alumnus.Remove(alumni);
+            
+        }
+
+    }
+}
---
+++ Q3Tests/AlumniTests.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Q3;
+
+namespace Q3Tests
+{
+    [TestClass]
+    public class AlumniTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CreateAlumniWithFutureGradDateShouldThrowArgumentOutOfRangeException()
+        {
+            DateTime futureDateTime = new DateTime(2200, 12, 1);
+
+            DateTime startDateTime = new DateTime(2015, 12, 1);
+
+            Alumni alumni = new Alumni("UG123", "Joe", "Stevens", futureDateTime, startDateTime);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CreateAlumniNumOfYrsBetweenStartAndGradMoreThan6YrsShouldThrowArugmentOutOfRangeException()
+        {
+            DateTime startDateTime = new DateTime(2001, 12, 1);
+
+            DateTime gradDateTime = DateTime.Now;
+
+            Alumni alumni = new Alumni("UGAC1", "Nikki", "Bell", gradDateTime, startDateTime);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CreateAlumniWithNullCourseCodeShouldThrowArgumentNullException()
+        {
+            DateTime startDateTime = new DateTime(2014, 12, 1);
+
+            DateTime gradDateTime = new DateTime(2015, 12, 2);
+
+            Alumni alumni = new Alumni(null, "Sarah", "Cornell", gradDateTime, startDateTime);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CreateAlumniWithIncorrectCourseCodeFormatShouldThrowArgumentException()
+        {
+            DateTime startDateTime = new DateTime(2013, 09, 11);
+
+            DateTime gradDateTime = new DateTime(2016, 10, 22);
+
+            Alumni alumni = new Alumni("BH101", "Dave", "McHale", gradDateTime, startDateTime);
+        }
+
+        [TestMethod]
+        public void AddAlumniUpdatesEmptyAlumnusWithNewAlumni()
+        {
+            DateTime startDateTime = new DateTime(2015, 12, 12);
+
+            DateTime gradDateTime = new DateTime(2016, 6, 6);
+
+            Alumni alumni = new Alumni("PG192E1", "Phil", "Davids", gradDateTime, startDateTime);
+
+            AlumnusList alumnus = new AlumnusList();
+
+            alumnus.AddAlumni(alumni);
+
+            CollectionAssert.Contains(alumnus.Alumnus, alumni);
+
+            /*
+             Above line is equivalent to the following...
+            List<Alumni> alums = new List<Alumni>();
+            alums.Add(alumni);
+
+            CollectionAssert.AreEquivalent(alumnus.Alumnus, alums);
+            */
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddNullAlumniToAlumnusListShouldThrowArgumentNullException()
+        {
+            Alumni alumni = null;
+
+            AlumnusList alumnus = new AlumnusList();
+
+            alumnus.AddAlumni(alumni);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddNullAlumniArrayToAlumnusListShouldThrowArgumentNullException()
+        {
+            Alumni[] nullAlumnus = null;
+
+            AlumnusList alumnus = new AlumnusList();
+
+            alumnus.AddAlumnus(nullAlumnus);
+        }
+
+        [TestMethod()]
+        public void AddAlumniArrayToAlumnusShouldContainArrayEntriesInAlumnusList()
+        {
+            DateTime startDateTime = new DateTime(2015, 12, 12);
+
+            DateTime gradDateTime = new DateTime(2016, 6, 6);
+
+            Alumni alumni1 = new Alumni("PG192E1", "Phil", "Davids", gradDateTime, startDateTime);
+            Alumni alumni2 = new Alumni("UG333F1", "Bazz", "Atron", gradDateTime, startDateTime);
+
+            Alumni[] alumniArray = new Alumni[2];
+
+            AlumnusList alumnus = new AlumnusList();
+
+            alumniArray.SetValue(alumni1, 0);
+            alumniArray.SetValue(alumni2, 1);
+
+            alumnus.AddAlumnus(alumniArray);
+
+            CollectionAssert.Contains(alumnus.Alumnus, alumni1);
+            CollectionAssert.Contains(alumnus.Alumnus, alumni2);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddAlumniArrayWithOneNullAlumniToAlumnusShouldThrowArgumentNullException()
+        {
+            DateTime startDateTime = new DateTime(2015, 12, 12);
+
+            DateTime gradDateTime = new DateTime(2016, 6, 6);
+
+            Alumni alumni1 = new Alumni("PG192E1", "Phil", "Davids", gradDateTime, startDateTime);
+            Alumni alumni2 = null;
+
+            Alumni[] alumniArray = new Alumni[2];
+
+            AlumnusList alumnus = new AlumnusList();
+
+            alumniArray.SetValue(alumni1, 0);
+            alumniArray.SetValue(alumni2, 1);
+
+            alumnus.AddAlumnus(alumniArray);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void RemoveFromEmptyAlumnusListShouldThrowArgumentException()
+        {
+            DateTime startDateTime = new DateTime(2015, 12, 12);
+            DateTime gradDateTime = new DateTime(2016, 6, 6);
+            Alumni alumni = new Alumni("PG192E1", "Phil", "Davids", gradDateTime, startDateTime);
+
+            AlumnusList alumnus = new AlumnusList();
+
+            alumnus.RemoveAlumni(alumni);
+        }
+
+        [TestMethod()]
+        public void RemoveAddedAlumniFromAlumnusListShouldRemoveAlumni()
+        {
+            DateTime startDateTime = new DateTime(2015, 12, 12);
+            DateTime gradDateTime = new DateTime(2016, 6, 6);
+            Alumni alumni = new Alumni("PG192E1", "Phil", "Davids", gradDateTime, startDateTime);
+            AlumnusList alumnus = new AlumnusList();
+            alumnus.AddAlumni(alumni);
+            //verify alumni was added in the first place
+            CollectionAssert.Contains(alumnus.Alumnus, alumni);
+
+            Assert.AreEqual(true, alumnus.RemoveAlumni(alumni));
+
+            //verify alumni removed
+            CollectionAssert.DoesNotContain(alumnus.Alumnus, alumni);
+        }
+    }
+}
